b090fef434b3f47fdb270824b61372f211526509
Hannes <hasnep@gmail.com>
2020-03-10 21:03:29 +0000

    Add formatting tests

---
+++ test/test_formatting.jl
@@ -0,0 +1,17 @@
+using Glob
+import JuliaFormatter
+using Test
+
+@testset "Test codebase is formtted correctly" begin
+    source_files = glob("../**/*.jl")
+    @testset "Test $filepath is formtted correctly" for filepath in source_files
+        file_string = read(filepath, String)
+        file_string_formatted = JuliaFormatter.format_text(
+            file_string,
+            always_for_in = true,
+            whitespace_typedefs = true,
+            whitespace_ops_in_indices = true,
+        )
+        @test file_string == file_string_formatted
+    end
+end
