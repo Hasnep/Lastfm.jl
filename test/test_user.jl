7660bde4946dcd7e192b443ee6ee3ef2b650445f
Hannes <hasnep@gmail.com>
2020-03-27 22:39:38 +0000

    Make all user tests active

--- test/test_user.jl
+++ test/test_user.jl
@@ -10,14 +10,14 @@ authenticate_lastfm(ENV["LASTFM_KEY"])
     @test user_get_friends(username_test) isa DataFrame
     @test user_get_info(username_test) isa DataFrame
     @test user_get_loved_tracks(username_test) isa DataFrame
-    @test_skip user_get_personal_tags(username_test, "rock", "track") isa DataFrame
+    @test user_get_personal_tags(username_test, "rock", "track") isa DataFrame
     @test user_get_recent_tracks(username_test) isa DataFrame
     @test user_get_top_albums(username_test) isa DataFrame
     @test user_get_top_artists(username_test) isa DataFrame
-    @test_skip user_get_top_tags(username_test) isa DataFrame
-    @test_skip user_get_top_tracks(username_test) isa DataFrame
-    @test_skip user_get_weekly_album_chart(username_test) isa DataFrame
-    @test_skip user_get_weekly_artist_chart(username_test) isa DataFrame
-    @test_skip user_get_weekly_chart_list(username_test) isa DataFrame
-    @test_skip user_get_weekly_track_chart(username_test) isa DataFrame
+    @test user_get_top_tags(username_test) isa DataFrame
+    @test user_get_top_tracks(username_test) isa DataFrame
+    @test user_get_weekly_album_chart(username_test) isa DataFrame
+    @test user_get_weekly_artist_chart(username_test) isa DataFrame
+    @test user_get_weekly_chart_list(username_test) isa DataFrame
+    @test user_get_weekly_track_chart(username_test) isa DataFrame
 end
