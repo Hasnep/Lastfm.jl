7b22326814ba8472f478788736231ee5edd91b7a
Hannes <hasnep@gmail.com>
2020-03-28 16:01:58 +0000

    Test get_personal_tags throws on invalid arg value

--- test/test_user.jl
+++ test/test_user.jl
@@ -14,6 +14,7 @@ authenticate_lastfm(ENV["LASTFM_KEY"])
         @test user_get_personal_tags(username_test, "indie", "artist") isa DataFrame
         @test user_get_personal_tags(username_test, "indie", "album") isa DataFrame
         @test user_get_personal_tags(username_test, "indie", "track") isa DataFrame
+        @test_throws AssertionError user_get_personal_tags(username_test, "indie", "NA")
     end
     @test user_get_recent_tracks(username_test) isa DataFrame
     @test user_get_top_albums(username_test) isa DataFrame
